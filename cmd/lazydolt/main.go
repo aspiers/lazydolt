1cfc85ca60647357ff45435bfc512a497ea6383f
Adam Spiers <dolt@adamspiers.org>
2026-03-07 12:56:49 +0000

    Scaffold Go project with Bubble Tea skeleton
    - go mod init github.com/aspiers/lazydolt
    - Directory structure: cmd/lazydolt/, internal/{dolt,domain,ui/components}/
    - .editorconfig for Go
    - Minimal main.go with Bubble Tea alt-screen, quit on q
    - Dependencies: bubbletea, lipgloss, bubbles
    
    Closes lazydolt-gl0

---
+++ cmd/lazydolt/main.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"fmt"
+	"os"
+
+	tea "github.com/charmbracelet/bubbletea"
+)
+
+type model struct {
+	quitting bool
+}
+
+func (m model) Init() tea.Cmd {
+	return nil
+}
+
+func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
+	switch msg := msg.(type) {
+	case tea.KeyMsg:
+		switch msg.String() {
+		case "q", "ctrl+c":
+			m.quitting = true
+			return m, tea.Quit
+		}
+	}
+	return m, nil
+}
+
+func (m model) View() string {
+	if m.quitting {
+		return ""
+	}
+	return "lazydolt\n\nPress q to quit.\n"
+}
+
+func main() {
+	p := tea.NewProgram(model{}, tea.WithAltScreen())
+	if _, err := p.Run(); err != nil {
+		fmt.Fprintf(os.Stderr, "Error: %v\n", err)
+		os.Exit(1)
+	}
+}
